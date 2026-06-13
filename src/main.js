6cc01531375827e8e66c77663b76f5ec93a9055a
Maria P <mariapyanykh@gmail.com>
2026-06-13 17:48:35 +0300

    import state.js, api.js (check)

--- src/main.js
+++ src/main.js
@@ -1,3 +1,14 @@
 import './style.css';
+import { state } from './state.js';
+import { getPacks } from './api.js';
+
+init();
+
+function init() {
+  console.log('App initialized');
+  console.log(state);
+
+  getPacks();
+}
 
 
